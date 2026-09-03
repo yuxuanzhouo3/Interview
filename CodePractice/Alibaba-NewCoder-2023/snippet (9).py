ce6f2fb57dd8378040bf9db736e4e30c843450dd
yuxuanzhouo3 <yuxuanzhouo3@gmail.com>
2026-09-03 11:14:25 +0800

    OO

--- CodePractice/Alibaba-NewCoder-2023/snippet (9).py
+++ CodePractice/Alibaba-NewCoder-2023/snippet (9).py
@@ -6,16 +6,19 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-pos = {x: i for i, x in enumerate(B)}
+indexB = {x: i for i, x in enumerate(B)}
 
 mx = cur = 1
 
+# 所有从头到尾一次都没有被取出的元素，在原 A 中必须是连续的一整段。
+# a b [c d e] f g; [c d e] 0 moves; b c and e f is not subarray;  a b f g at least move once
+
 for i in range(1, N):
-    if pos[A[i]] > pos[A[i - 1]]:
+    if indexB[A[i]] > indexB[A[i - 1]]:
         cur += 1
     else:
         cur = 1
 
     mx = max(mx, cur)
 
-print(N - mx)
+print(N - mx) 
